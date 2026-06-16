99fb59c5b229488782437e6b4c474bf0f9cf9c61
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-16 16:33:06 -0500

    pass reverse string tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {  //function with parameters defined as words & times
+    
+    let reversed = string.split("").reverse().join(""); //splits the string into an array, reverses array, joins it back into a string
+    
+    return reversed;
 
+    //can be condensed into - return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
