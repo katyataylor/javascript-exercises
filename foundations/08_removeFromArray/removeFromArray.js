4fcd1d382c199377cd687c290922f1d997a7666d
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-17 18:09:14 -0500

    pass array tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];  //creates an empty array container
+
+    array.forEach((item) => {
+        //uses forEach to push every element into newArray
+        //UNLESS included in function args
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+
+    //Smarter way
+    //const removeFromArray = function(array, ...args){
+    //  return array.filter(val => !args.includes(val))
+    //}
 };
 
 // Do not edit below this line
