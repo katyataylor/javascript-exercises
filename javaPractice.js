3e4eb36dda57f08b896865ce06e00440c3f615b8
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-22 14:13:58 -0500

    establish Number guessing game logic in javascript

--- javaPractice.js
+++ javaPractice.js
@@ -1,21 +1,9 @@
-/* example of setting up an interactive element in javascript.*/
+// Number guessing game 
 
-// function createParagraph() {
-//     const para = document.createElement("p");
-//     para.textContent = "You clicked the button!";
-//     document.body.appendChild(para);
-// }
+let randomNumber = Math.floor(Math.random() * 100) + 1; //This equation determines a random number
 
-// const buttons = document.querySelectorAll("button");
-
-// for (const button of buttons) {
-//     button.addEventListener("click", createParagraph);
-// }
-
-// Number guessing game code below 
-
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-
+//Defining variables that allow the game to be broken down into different parts
+//with different actions.
 const guesses = document.querySelector(".guesses");
 const lastResult = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector(".lowOrHi");
@@ -23,9 +11,90 @@ const lowOrHi = document.querySelector(".lowOrHi");
 const guessSubmit = document.querySelector(".guessSubmit");
 const guessField = document.querySelector(".guessField");
 
-let guessCount = 1;
+let guessCount = 1; //Default count 
 let resetButton;
 
+//stores number entered into guessField, makes sure the value is between 1 & 100,
+//sets up a guessCount with diplayed related info, gives user feedback on guess.
 function checkGuess() {
-    console.log("I am a placeholder");
+    
+    const userGuess = Number(guessField.value);
+
+    // VALIDATION RULE: Stop numbers above 100 or below 1
+    if (userGuess > 100 || userGuess < 1 || isNaN(userGuess)) {
+        alert("Please enter a valid number between 1 and 100!");
+        guessField.value = "";
+        guessField.focus();
+        return; // This exits the function early so no turn is counted!
+    }
+
+    if (guessCount === 1) {
+        guesses.textContent = "Previous guesses: ";
+    }
+    guesses.textContent += ` ${userGuess}`;
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "Congratulations! You got it right!";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = "";
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = "!!!GAME OVER!!!";
+        lowOrHi.textContent = "";
+        setGameOver();
+    } else {
+        lastResult.textContent = "Wrong!";
+        lastResult.style.backgroundColor = "red";
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = "Last guess was too low!";
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = "Last guess was too high!";
+        }
+    }
+
+    guessCount++;
+    guessField.value = "";
+    guessField.focus();
 };
+
+guessSubmit.addEventListener("click", checkGuess); 
+
+// This lets users submit guesses by hitting the "Enter" key
+guessField.addEventListener ("keydown", (event) => {
+    if (event.key === "Enter") {
+        checkGuess();
+    }
+});
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Start new game";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+
+    const resetParas = document.querySelectorAll(".resultParas p");
+    for (const resetPara of resetParas) {
+        resetPara.textContent = "";
+    }
+
+    guesses.textContent = "";
+    lastResult.textContent = "";
+    lowOrHi.textContent = "";
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    guessField.focus();
+
+    lastResult.style.backgroundColor = "";
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
