30324c52335fe59dee59e85f02525225cdd85490
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-23 08:43:11 -0500

    pass conversion tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32)/1.8;
+  return +tempC.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = ((tempC * 1.8)+32);
+  return +tempF.toFixed(1);
 };
 
 // Do not edit below this line
