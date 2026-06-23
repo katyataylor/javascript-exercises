b7a3f3333108a41024350fcb05def992c4f5d4e7
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-22 19:56:58 -0500

    pass sum logic tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    //return error alert with negative numbers
+    if (a < 0 || b < 0 
+        || isNaN(a) || isNaN(b) 
+        || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+
+    //differentiates between smaller and larger number
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+
+    //Variable that holds the running total
+    let totalSum = 0;
+
+    //loops through every integer
+    for (let i = start; i <= end; i++) {
+        totalSum += i;
+    }
+
+    //return sum
+    return totalSum;
 
 };
 
