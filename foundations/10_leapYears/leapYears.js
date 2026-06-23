6da511e2a241bd619eb4c860e4dc77f1f09b3e66
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-23 07:25:04 -0500

    pass equation logic tests

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //checks if the remainder of a year divisible by 4 is exactly 0,
+    //then makes sure the remainder is 0 & not divisible by 100, but is by 400
+    return (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0); 
 };
 
 // Do not edit below this line
