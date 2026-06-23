3025cffafaba4e94479d8feec09939c059bd3350
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-23 10:34:46 -0500

    detect palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let cleanInput = input.toLowerCase().replace(/[^a-zA-Z0-9]/g, ``);
+    let reversed = cleanInput.split(``).reverse().join(``);
+    if (reversed === cleanInput) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
