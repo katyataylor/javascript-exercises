387e52c1adf305b3c30c5fb58d6ab744f607ef98
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-24 09:59:53 -0500

    establish fibonacci count loop

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+
+    // Checks for edge cases first and returns what test expects
+    const num = Number(count);
+    if (num < 0 || Number.isNaN(num)) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+    
+    // Starts off with array and runs loop that adds to the sequence as long as the
+    // array length is shorter than the count desired by test
+    const fibArray = [1,1];
+    while (count >= fibArray.length) {
+        const secondLast = fibArray.at(-2);
+        const last = fibArray.at(-1);
+        fibArray.push(Number(secondLast + last));
+    }
+
+    // returns [count - 1] because javascript counting starts at 0, not 1
+    return fibArray[count - 1];
 
 };
 
