854f61538f8c02b6a7599ae7691a0190f0d28ee9
kat.taylor <sofia.katya.taylor@gmail.com>
2026-06-24 13:23:07 -0500

    get age from arrays, filter for oldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  // If yearOfDeath is undefined, use the current year
+  const deathYear = person.yearOfDeath || new Date().getFullYear();
+  return deathYear - person.yearOfBirth;
+};
 
+
+const findTheOldest = function(people) {
+
+    const oldestPerson = people.reduce((currentOldest,nextPerson) => {
+
+        const currentAge = getAge(currentOldest);
+        const nextAge = getAge(nextPerson);
+
+        if (nextAge > currentAge) {
+            return nextPerson;
+        } else {
+            return currentOldest;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
